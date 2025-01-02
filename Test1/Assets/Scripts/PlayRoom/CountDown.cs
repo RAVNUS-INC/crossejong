27b274606f894018c80d5f897adb29d59b918ec8
hyewon0170 <profileme0170@gmail.com>
2025-01-02 17:53:32 +0900

    카운트다운 백업

--- Test1/Assets/Scripts/PlayRoom/CountDown.cs
+++ Test1/Assets/Scripts/PlayRoom/CountDown.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro; // TextMeshPro를 사용하기 위한 네임스페이스
 using System.Collections;
 
 public class Countdown : MonoBehaviour
 {
-    public Text countDownText; // 카운트다운 텍스트
+    public TMP_Text countDownText; // TextMeshPro 사용
     public StartCard startCard; // StartCard 참조
     public UserCard userCard;  // UserCard 참조
     public float startDelay = 1f; // 시작 딜레이
@@ -21,13 +21,13 @@ public class Countdown : MonoBehaviour
         // 카운트다운 표시
         while (count > 0)
         {
-            countDownText.text = count.ToString();
+            countDownText.text = count.ToString(); // TMP_Text로 설정
             yield return new WaitForSeconds(1f);
             count--;
         }
 
         // "시작!" 표시
-        countDownText.text = "시작!";
+        countDownText.text = "Start!"; // TMP_Text로 설정
         yield return new WaitForSeconds(startDelay);
 
         countDownText.gameObject.SetActive(false); // 카운트다운 텍스트 숨김
