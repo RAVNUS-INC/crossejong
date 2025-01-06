d68cd9e3e50fe5eb7462e40a9e7eea0b3c7cc439
seunghyun5712 <you05070507@gmail.com>
2025-01-07 03:03:19 +0900

    메인 UI 수정 및 닉네임, 방이름 규칙설정

--- Test1/Assets/Scripts/Connection/ConnectionManager.cs
+++ Test1/Assets/Scripts/Connection/ConnectionManager.cs
@@ -2,6 +2,7 @@ using Photon.Pun;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -14,30 +15,74 @@ public class ConnectionManager : MonoBehaviourPunCallbacks
     public ConnectionManager Instance { get { return s_instance; } }
 
     [SerializeField]
-    InputField inputText;
+    InputField inputText; //´Ğ³×ÀÓ ÀÔ·Â
     [SerializeField]
-    Button inputButton;
+    Button confirmButton; //Á¦Ãâ ¹öÆ°
+    [SerializeField]
+    Text warningText; // °æ°í ¸Ş½ÃÁö¸¦ Ãâ·ÂÇÒ UI ÅØ½ºÆ®
+
+    private const int MaxLength = 5; // ÃÖ´ë ÀÔ·Â ±æÀÌ
 
     void Start()
     {
+        confirmButton.interactable = false; // ±âº»ÀûÀ¸·Î ¹öÆ° ºñÈ°¼ºÈ­
+        warningText.text = ""; // ÃÊ±â °æ°í ¸Ş½ÃÁö ºñ¿ì±â
+
         //³»¿ëÀÌ º¯°æµÇ¾úÀ»¶§
-        inputText.onValueChanged.AddListener(OnValueChanged);
+        inputText.onValueChanged.AddListener(ValidateNickname);
+
         //³»¿ëÀ» Á¦ÃâÇßÀ»¶§
         inputText.onSubmit.AddListener(OnSubmit);
-        //Ä¿¼­°¡ ´Ù¸¥°÷À» ´©¸£¸é
-        inputText.onEndEdit.AddListener(
-            (string s) =>
-            {
-                Debug.Log("OnEndmit" + s);
-            }
-        );
-        inputButton.onClick.AddListener(OnClickConnect);
+
+        ////Ä¿¼­°¡ ´Ù¸¥°÷À» ´©¸£¸é
+        //inputText.onEndEdit.AddListener(
+        //    (string s) =>
+        //    {
+        //        Debug.Log("OnEndmit" + s);
+        //    }
+        //);
+
+        confirmButton.onClick.AddListener(OnClickConnect);
     }
 
-    void OnValueChanged(string s) // s´Â ¹®ÀÚ¿­
+    private void ValidateNickname(string input)
     {
-        inputButton.interactable = s.Length > 0; // input¿¡ ¹¹¶óµµ ÀÔ·ÂÇßÀ¸¸é È®ÀÎ¹öÆ° È°¼ºÈ­
+        /// ÇÑ±Û(¿Ï¼ºÇü/ÀÚÀ½/¸ğÀ½)°ú ¼ıÀÚ¸¸ Çã¿ëÇÏ´Â Á¤±Ô½Ä
+        string validPattern = @"^[°¡-ÆR¤¡-¤¾¤¿-¤Ó0-9]*$";
+
+        // °ø¹é Á¦°Å
+        input = input.Replace(" ", "");
+
+        // ÀÔ·Â °ªÀÌ ÆĞÅÏ¿¡ ¸ÂÁö ¾ÊÀ¸¸é ¼öÁ¤
+        if (!Regex.IsMatch(input, validPattern))
+        {
+            warningText.text = "ÇÑ±Û°ú ¼ıÀÚ¸¸ ÀÔ·Â °¡´ÉÇÕ´Ï´Ù.";
+            confirmButton.interactable = false; // È®ÀÎ ¹öÆ° ºñÈ°¼ºÈ­
+        }
+        else if (input.Length > MaxLength) // ±æÀÌ Á¦ÇÑ ÃÊ°ú °Ë»ç
+        {
+            warningText.text = $"ÃÖ´ë {MaxLength}ÀÚ±îÁö¸¸ ÀÔ·Â °¡´ÉÇÕ´Ï´Ù.";
+            confirmButton.interactable = false; // È®ÀÎ ¹öÆ° ºñÈ°¼ºÈ­
+        }
+        else if (input.Length == 0) // ºó ¹®ÀÚ¿­ °Ë»ç
+        {
+            warningText.text = "´Ğ³×ÀÓÀ» ÀÔ·ÂÇØÁÖ¼¼¿ä.";
+            confirmButton.interactable = false; // È®ÀÎ ¹öÆ° ºñÈ°¼ºÈ­
+        }
+        else
+        {
+            warningText.text = ""; // ±ÔÄ¢¿¡ ¸ÂÀ¸¸é °æ°í ¸Ş½ÃÁö Á¦°Å
+            confirmButton.interactable = true; // È®ÀÎ ¹öÆ° È°¼ºÈ­
+        }
+
     }
+
+    private void OnDestroy()
+    {
+        // ÀÌº¥Æ® ÇØÁ¦
+        inputText.onValueChanged.RemoveListener(ValidateNickname);
+    }
+
     void OnSubmit(string s) // s´Â ¹®ÀÚ¿­
     {
         Debug.Log("OnSubmit " + s); // ´Ğ³×ÀÓÀ» ÀÔ·ÂÇÏ°í Á¦ÃâÇßÀ½À» ¾Ë¸²
@@ -50,9 +95,11 @@ public class ConnectionManager : MonoBehaviourPunCallbacks
 
         //³ªÀÇ ÀÌ¸§À» Æ÷Åæ¿¡ ¼³Á¤
         PhotonNetwork.NickName = inputText.text;
+
         //·ÎºñÁøÀÔ
         PhotonNetwork.JoinLobby();
     }
+
     //Lobby ÁøÀÔ¿¡ ¼º°øÇßÀ¸¸é È£ÃâµÇ´Â ÇÔ¼ö
     public override void OnJoinedLobby()
     {
