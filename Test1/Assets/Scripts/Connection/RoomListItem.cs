b7aa0ea13e9c9da12edf6f90375c52c4a0943f19
seunghyun5712 <you05070507@gmail.com>
2025-01-13 02:11:48 +0900

    방 생성 후 텍스트, 버튼 업데이트

--- Test1/Assets/Scripts/Connection/RoomListItem.cs
+++ Test1/Assets/Scripts/Connection/RoomListItem.cs
@@ -16,6 +16,7 @@ public class RoomListItem : MonoBehaviour
     public void SetInfo(string roomName, int currPlayer, int maxPlayer, string difficulty, int timeLimit)
     {
         name = roomName;
+
         // 텍스트 업데이트: 첫 줄에 방 이름과 인원, 두 번째 줄에 난이도와 제한 시간
         roomInfo.text = $"{roomName} ({currPlayer}/{maxPlayer})\n" + $"{maxPlayer}명 / {difficulty} / {timeLimit}초";
     }
@@ -27,11 +28,5 @@ public class RoomListItem : MonoBehaviour
         {
             onDelegate(name);
         }
-        ////InputRoomName 찾아오기
-        //GameObject go =GameObject.Find("InputRoomName");
-        ////찾아온 게임오브젝트에서 InputField 컴포넌트 가져오기
-        //InputField inputField = go.GetComponent<InputField>();
-        ////가져온 컴포넌트에서 text 값을 나의 이름으로 셋팅하기
-        //inputField.text = name;
     }
 }
