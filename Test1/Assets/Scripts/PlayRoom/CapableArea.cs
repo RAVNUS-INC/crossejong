d6892c7bd971b9e668e59d097828d5622f3f03ea
hyewon0170 <profileme0170@gmail.com>
2025-01-22 14:31:23 +0900

    보유카드 전체보기 구현

--- Test1/Assets/Scripts/PlayRoom/CapableArea.cs
+++ Test1/Assets/Scripts/PlayRoom/CapableArea.cs
@@ -10,5 +10,17 @@ using Unity.Collections.LowLevel.Unsafe;
 
 public class CapableArea : MonoBehaviour
 {
+    public FieldCard fieldCard;
 
+    public void ShowCapableArea()
+    {
+        if (fieldCard != null) 
+        {
+            for (int i = 0; i < fieldCard.fieldDisplayedCards.Count; i++)
+            {
+                RectTransform rectTransform = fieldCard.fieldDisplayedCards[i].GetComponent<RectTransform>();
+                
+            }
+        }
+    }
 }
