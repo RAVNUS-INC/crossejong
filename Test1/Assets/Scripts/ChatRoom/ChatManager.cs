32903071a02abef70c9060b402ef52310a5719f1
seunghyun5712 <you05070507@gmail.com>
2025-01-24 21:54:58 +0900

    chatRoomSet 함수 정리

--- Test1/Assets/Scripts/ChatRoom/ChatManager.cs
+++ Test1/Assets/Scripts/ChatRoom/ChatManager.cs
@@ -78,27 +78,6 @@ public class ChatManager : MonoBehaviour
             Area.UserText.text = Area.User;
         }
 
-        //이전 것과 날짜가 다르면 날짜 영역 보이기
-        //if (LastArea != null && LastArea.Time.Substring(0, 10) != Area.Time.Substring(0, 10))
-        //{
-        //    Transform CurDateArea = Instantiate(DateArea).transform;
-        //    CurDateArea.SetParent(ContentRect.transform, false);
-        //    CurDateArea.SetSiblingIndex(CurDateArea.GetSiblingIndex() - 1);
-
-        //    string week = "";
-        //    switch (t.DayOfWeek)
-        //    {
-        //        case DayOfWeek.Sunday: week = "일"; break;
-        //        case DayOfWeek.Monday: week = "월"; break;
-        //        case DayOfWeek.Tuesday: week = "화"; break;
-        //        case DayOfWeek.Wednesday: week = "수"; break;
-        //        case DayOfWeek.Thursday: week = "목"; break;
-        //        case DayOfWeek.Friday: week = "금"; break;
-        //        case DayOfWeek.Saturday: week = "토"; break;
-        //    }
-        //    CurDateArea.GetComponent<AreaScript>().DateText.text = t.Year + "년" + t.Month + "월" + t.Day + "일" + week + "요일";
-        //}
-
         Fit(Area.BoxRect);
         Fit(Area.AreaRect);
         Fit(ContentRect);
