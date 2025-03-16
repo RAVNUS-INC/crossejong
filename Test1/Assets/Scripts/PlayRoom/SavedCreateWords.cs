fe4786a93159db050caf83508e4d664c0d14782f
hyewon0170 <profileme0170@gmail.com>
2025-03-17 02:10:59 +0900

    난이도 백업

--- Test1/Assets/Scripts/PlayRoom/SavedCreateWords.cs
+++ Test1/Assets/Scripts/PlayRoom/SavedCreateWords.cs
@@ -2,39 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using System.IO;
+using System.Text;
+using UnityEditor.ShaderKeywordFilter;
 
 public class SavedCreateWords : MonoBehaviour
 {
-    public DictionaryAPI dictionaryAPI;
-    public Dictionary<string, int> createdWordCounts = new Dictionary<string, int>();
-
-    public void SaveCreatedWord(string word)  // 단어 저장 및 카운트
+    public string csvFileName = "우왕좌왕세종대왕";  // 읽어 올 파일 이름
+    public Dictionary<string, int> playerCreateWords = new Dictionary<string, int>();  // 플레이어가 만든 단어와 횟수
+    public string _playerCreateWord;
+    public int _playerCreateWordNum;
+    public string path = "우왕좌왕세종대왕.csv";  //파일 이름.확장자
+    public bool isFinish = false;  // 마지막 줄을 판별하기 위한 bool 타입 변수
+    private void Start()
     {
-        if (createdWordCounts.ContainsKey(word))
+        ReadCSV();
+    }
+    private void ReadCSV()
+    {
+        StreamReader reader = new StreamReader(Application.dataPath + "/" + path);  // UTF-8 인코딩을 위한 StreamReader
+
+        while(isFinish == false)
         {
-            createdWordCounts[word]++;
-        }
-        else
-        {
-            createdWordCounts[word] = 1;
+            // 한 줄씩 읽어서 string으로 반환하는 메서드
+            string data = reader.ReadLine();  // 한 줄 읽기
+
+            // data 변수가 비었는지 확인
+            if(data == null)
+            {
+                // 만약 비었다면, 마지막 줄은 데이터가 없음
+                isFinish = true;
+                break;
+            }
+            
         }
     }
-
-    public List<KeyValuePair<string, int>> GetTop10Words()  // 상위 10개 단어 출력
-    {
-        return createdWordCounts.OrderByDescending(x => x.Value).Take(10).ToList();
-    }
-
-    //IEnumerator ProcessDictionaryCheck(string word)
-    //{
-    //    yield return StartCoroutine(dictionaryAPI.CheckWordExists(word));
-
-    //    if (dictionaryAPI.isWordValid) // 사전 검사 성공 시
-    //    {
-    //        SaveCreatedWord(word);  // 단어 저장
-    //        Debug.Log($"단어 저장 완료: {word}, 사용 횟수: {createdWordCounts[word]}");
-    //    }
-    //}
-
 
 }
