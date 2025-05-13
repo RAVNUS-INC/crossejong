e0ef342bba82400dd82a8d07506ba69b1bcac9e6
hyewon0170 <profileme0170@gmail.com>
2025-05-13 15:03:30 +0900

    롤백 애니메이션 백업

--- Test1/Assets/Scripts/PlayRoom/CardAnimation.cs
+++ Test1/Assets/Scripts/PlayRoom/CardAnimation.cs
@@ -6,9 +6,12 @@ using Photon.Pun;
 using TMPro;
 using Unity.VisualScripting;
 using static UnityEngine.GraphicsBuffer;
+using UnityEngine.UI;
 
 public class CardAnimation : MonoBehaviourPun
 {
+    public Button getCardButton;
+
     public static CardAnimation instance;
 
     public CanvasGroup imageCanvasGroup;  // 투명도 제어
@@ -16,11 +19,25 @@ public class CardAnimation : MonoBehaviourPun
 
     public Transform addStartPos; // 카드 추가 시작 위치
     public Transform RollFinPos; // 나 - 롤백 종료 위치
-    public Transform[] AllPlayerPos; // 플레이어 프로필 위치
+    //public Transform[] AllPlayerPos; // 플레이어 프로필 위치
     public Transform FieldCenPos; // 보드 중앙 - 롤백 시작 위치
+    public Transform playerPos1;
+    public Transform playerPos2;
+    public Transform playerPos3;
+    public Transform playerPos4;
+    public Transform playerPos5;
+
     Vector3 addStartPosV;
     Vector3 RollFinPosV;
     Vector3 FieldCenPosV;
+    Vector3 playerPos1V;
+    Vector3 playerPos2V;
+    Vector3 playerPos3V;
+    Vector3 playerPos4V;
+    Vector3 playerPos5V;
+
+    public List<Vector3> playerPosList;
+
     //private Vector3 ZeroPosition = new Vector3(-396, -532, 0); // 처음 위치(카드 추가 버튼)
     //private Vector3 RollPosition = new Vector3(0, 110, 0); // 롤백 초기 위치(보드판의 중앙)
 
@@ -48,6 +65,16 @@ public class CardAnimation : MonoBehaviourPun
         addStartPosV = addStartPos.position;
         RollFinPosV = RollFinPos.position;
         FieldCenPosV = FieldCenPos.position;
+        playerPos1V = playerPos1.transform.position;
+        playerPos2V = playerPos2.transform.position;
+        playerPos3V = playerPos3.transform.position;
+        playerPos4V = playerPos4.transform.position;
+        playerPos5V = playerPos5.transform.position;
+
+        playerPosList = new List<Vector3>()
+        {
+            playerPos1V, playerPos2V, playerPos3V, playerPos4V, playerPos5V
+        };
 
         //Debug.Log($"추가시작위치: {addStartPosV}");
         //Debug.Log($"롤백끝위치: {RollFinPosV}");
@@ -62,6 +89,8 @@ public class CardAnimation : MonoBehaviourPun
     [PunRPC]
     public void AddCardAnimation(int userActNum) //누군가 카드를 먹으면 해당 유저를 향한 애니메이션을 취함  ok
     {
+        addStartPosV = getCardButton.transform.position;
+
         // 이미지를 활성화하고 애니메이션 시작
         imageTransform.gameObject.SetActive(true);
         imageTransform.position = addStartPosV; // 위치 초기화
@@ -71,7 +100,7 @@ public class CardAnimation : MonoBehaviourPun
 
         // 게임 UI내에서의 인덱스 번호가 i와 같다면
         // 이동 애니메이션 수행
-        imageTransform.DOAnchorPos(AllPlayerPos[userActNum].position, duration) // 앵커포지션
+        imageTransform.DOAnchorPos(playerPosList[userActNum], duration) // 앵커포지션
             .SetEase(Ease.OutQuad);
 
         // 이동이 끝날 때 투명도 감소 (1 → 0)
@@ -97,7 +126,7 @@ public class CardAnimation : MonoBehaviourPun
 
         // 게임 UI내에서의 인덱스 번호가 i와 같다면
         // 이동 애니메이션 수행
-        imageTransform.DOAnchorPos(AllPlayerPos[userActNum].position, duration) // 앵커포지션
+        imageTransform.DOAnchorPos(playerPosList[userActNum], duration) // 앵커포지션
             .SetEase(Ease.OutQuad);
 
         // 이동이 끝날 때 투명도 감소 (1 → 0)
